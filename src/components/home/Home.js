39b570565d6d6dd2515319b73d8ce5e095c4053a
Ben Whittle <benwhittle31@gmail.com>
2017-12-30 11:38:01 -0500

    added login with github button and beta access code input

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-
 import Ajax from '../../utils/Ajax';
+import FormInput from '../shared/form/FormInput';
 
 
 class Home extends Component {
 
-  componentDidMount() {
-    Ajax.get('/repos/dashboard')
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
+  constructor(props) {
+    super(props);
+    this.setBetaCodeInputRef = this.setBetaCodeInputRef.bind(this);
+    this.loginWithGithub = this.loginWithGithub.bind(this);
+  }
+
+  setBetaCodeInputRef(ref) {
+    this.betaCodeInput = ref;
+  }
+
+  loginWithGithub() {
+    if (!this.betaCodeInput.isValid()) {
+      return;
+    }
+
+    const betaCode = this.betaCodeInput.serialize();
+
+    Ajax.get('/api/github/oauth_url', { 'betaCode': betaCode })
+      .then((resp) => {
+        if (resp.status === 200) {
+          resp.json().then((data) => {
+            if (data.url) {
+              window.location = data.url;
+            }
+          });
+        } else {
+          resp.json().then((data) => {
+            if (data.error) {
+              console.error(data.error);
+            } else {
+              console.error('Unknown error while requesting OAuth url.');
+            }
+          });
+        }
       });
   }
 
   render() {
     return (
       <div id="home">
-        <div>Main Content</div>
+        <div className="content">
+          <FormInput placeholder="Beta Access Code" required={true} ref={this.setBetaCodeInputRef}/>
+          <button onClick={this.loginWithGithub}>Login with GitHub</button>
+        </div>
       </div>
     );
   }
